6765ee7130ea1345676d0bfede51332c07616ab4
Alexis <cv.alexisjesus@gmail.com>
2020-11-02 18:37:14 +0100

    refactor: image fixed

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,7 +6,6 @@ export default function Pokemon (keyword) {
 
   const [pokemon, setPokemon] = useState([])
 
-
   useEffect(() => {
     (
       async () => {
@@ -16,37 +15,52 @@ export default function Pokemon (keyword) {
     )()
 
   },[keyword])
-  const {keyword: text} = keyword
- 
+
   if (pokemon.sprites) {
 
     // eslint-disable-next-line no-unused-vars
     const {other, versions, ...spritesData } = pokemon.sprites
     pokemon.sprites = spritesData
   }
+
+
+  if (!keyword.keyword){
+    return null
+  }
+
+  if (pokemon === 'Not found') {
+    return (
+    <div id="pokemon">
+      <div className="error">
+            <p>Not found</p> 
+        </div>
+    </div>
+    )
+  }
   
   return(
     <div id='pokemon'>
-      {pokemon === 'Not found' || text.length <= 0
-      
-      ? <div className="error">
-        <p>Not found</p>
-       </div>
-
-       
-      :  <div className="data">
-          <p><strong>pokedex number:</strong> {pokemon.id}</p>
-          <p><strong>name:</strong> {pokemon.name}</p>
-          <p><strong>height: </strong>{pokemon.height / 10} m</p>
-          <p><strong>weight: </strong>{pokemon.weight / 10} kg</p>
+      <div className="data">
+          <p><strong>Pokedex number:</strong> {pokemon.id}</p>
+          <p><strong>Name:</strong> {pokemon.name}</p>
+          <p><strong>Height: </strong>{pokemon.height / 10} m</p>
+          <p><strong>Weight: </strong>{pokemon.weight / 10} kg</p>
           {pokemon.types && pokemon.types.map((el, index) => <p key={index}><strong>Type:</strong> {el.type.name}</p>)}
-        <div className="sprites">
+        <div className="default">
           {pokemon.sprites && Object.entries(pokemon.sprites).map(([key, val]) => {
-            if(val !== null) return <img key={key} src={val} alt={key}/>
+            if((key === 'front_default' || key === 'back_default') && val !== null) {
+              return <img key={key} src={val} alt={key}/>
+            }
+          })}
+        </div>
+        <div className="shiny">
+          {pokemon.sprites && Object.entries(pokemon.sprites).map(([key, val]) => {
+            if((key === 'front_shiny' || key === 'back_shiny') && val !== null) {
+              return <img key={key} src={val} alt={key}/>
+            }
           })}
         </div>
       </div> 
-    }
     </div>
   )
 }
